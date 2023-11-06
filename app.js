d19bebf5141c6215684602249a48b8d7dfd950ad
joseluismolina75 <joseluismolina75@gmail.com>
2023-11-06 03:13:29 -0300

    actualizar lo hecho en casa el finde

--- app.js
+++ app.js
@@ -6,8 +6,49 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.get('/', (req, res) => {
-        res.send('Hola Mundo');
+    app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'babbage.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'berners-lee.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'clarke.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'hamilton.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'hopper.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    });
+
+        app.get('/', (req, res) => {
+        //res.send('Hola Mundo');
+        res.sendFile(path.join(__dirname, 'views', 'index.html'));
     });
 
 const port = 3030
